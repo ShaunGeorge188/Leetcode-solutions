f08d9ac13b0318633bdee41c135e77a84c808124
Shaun George <shaungeorge2006@gmail.com>
2026-08-01 21:13:03 +0530

    Time: 0 ms (100%), Space: 95.2 MB (42.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size()) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        int low = 0, high = m;
-        
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = (m + n + 1) / 2 - partitionX;
-            
-            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-            int minRightX = (partitionX == m) ? INT_MAX : nums1[partitionX];
-            
-            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-            int minRightY = (partitionY == n) ? INT_MAX : nums2[partitionY];
-            
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                if ((m + n) % 2 == 1) {
-                    return max(maxLeftX, maxLeftY);
-                } else {
-                    return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
-                }
-            } else if (maxLeftX > minRightY) {
-                high = partitionX - 1;
-            } else {
-                low = partitionX + 1;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
+        int n = n1 + n2;
+
+        while(low <= high){
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(n % 2 == 1) return max(l1, l2);
+                return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }else if(l1 > r2){
+                high  = mid1 - 1;
+            }else{
+                low = mid1 + 1;
             }
         }
-        
-        return 0.0;
+        return 0;
     }
 };
