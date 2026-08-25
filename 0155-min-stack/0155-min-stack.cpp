4bb747c3c40f2bf86fdda8855d0680acf274d5c8
Shaun George <shaungeorge2006@gmail.com>
2026-08-25 12:17:26 +0530

    Time: 64 ms (39.16%), Space: 152.1 MB (23.76%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,14 @@
+class MinStack {
+    stack<pair<int,int>> st;   // {value, min-so-far}
+public:
+    void push(int val) {
+        int currMin = st.empty() ? val : min(val, st.top().second);
+        st.push({val, currMin});
+    }
+
+    void pop() { st.pop(); }
+
+    int top() { return st.top().first; }
+
+    int getMin() { return st.top().second; }
+};
